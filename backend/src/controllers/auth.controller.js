c08d7f08bcd46d1f42f08d13e078aed33ecf17b2
Amit Kushwaha <amitcoding56@gmail.com>
2025-05-06 10:35:34 +0530

    login and logout logic done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import bcrypt from "bcryptjs";
 import { db } from "../libs/db.js";
-import { UserRole } from "../generated/prisma/index.js";
+import { PrismaClient, UserRole } from "../generated/prisma/index.js";
 import jwt from "jsonwebtoken";
+
+// Register login
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -41,6 +43,7 @@ export const register = async (req, res) => {
     });
 
     res.status(201).json({
+      success: true,
       message: "User created successfully",
       user: {
         id: newUser.id,
@@ -58,8 +61,80 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
+// Login logic
 
-export const logout = async (req, res) => {};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await db.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        error: "user not found",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        error: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7, //7 days
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        image: user.image,
+      },
+    });
+  } catch (error) {
+    console.error("error logging in user", error);
+    res.status(500).json({ error: "error logging in user" });
+  }
+};
+
+// Logout logic
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+    });
+
+    res.status(204).json({
+      success: true,
+      message: "User logged out successfully",
+    });
+  } catch (error) {
+    console.error("error in logging out user", error);
+    res.status(500).json({
+      error: "Error in logging out user",
+    });
+  }
+};
+
+// Check logic
 
 export const check = async (req, res) => {};
