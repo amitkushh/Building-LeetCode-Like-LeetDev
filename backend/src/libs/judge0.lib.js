72859c78c1f9ca261c5e423e1e1e1856014b0d9e
Amit Kushwaha <amitcoding56@gmail.com>
2025-05-18 14:09:42 +0530

    implement judge0 polling batch results and submit batch

--- backend/src/libs/judge0.lib.js
+++ backend/src/libs/judge0.lib.js
@@ -7,3 +7,40 @@ export const getJudge0LanguageId = (language) => {
 
   return languageMap[language.toUpperCase()];
 };
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms)); //ms = milliseconds
+
+export const pollingBatchResults = async (tokens) => {
+  while (true) {
+    const { data } = await axios.gte(
+      `${process.env.JUDGE0_API_URL}/submissions/batch`,
+      {
+        params: {
+          tokens: tokens.join(","),
+          base64_encoded: false,
+        },
+      }
+    );
+
+    const results = data.submissions;
+
+    const isAllDone = results.every(
+      (r) => r.status.id !== 1 && r.status.id !== 2
+    );
+    if (isAllDone) return results;
+
+    await sleep(1000);
+  }
+};
+
+export const submitBatch = async (submissions) => {
+  const { data } = await axios.post(
+    `${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`,
+    {
+      submissions,
+    }
+  );
+
+  console.log("Submission Results", data);
+  return data;
+};
