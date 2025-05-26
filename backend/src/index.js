e988b1233ee1be64b868acc824769f793d49d9e6
Amit Kushwaha <amitcoding56@gmail.com>
2025-05-26 14:02:12 +0530

    Add submission routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,14 @@ import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import problemRoutes from "./routes/problem.routes.js";
 import executionRoute from "./routes/executeCode.routes.js";
+import submissionRoutes from "./routes/submission.routes.js";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("welcome to home page");
@@ -18,8 +19,9 @@ app.get("/", (req, res) => {
 
 //All Routes
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/problems", problemRoutes)
-app.use("/api/v1/execute-code", executionRoute)
+app.use("/api/v1/problems", problemRoutes);
+app.use("/api/v1/execute-code", executionRoute);
+app.use("/api/v1/submission", submissionRoutes);
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
