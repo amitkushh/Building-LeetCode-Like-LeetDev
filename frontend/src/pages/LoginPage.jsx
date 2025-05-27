b5b1f3ea44390a3d231ece6c8b03935e0eba125c
Amit Kushwaha <amitcoding56@gmail.com>
2025-05-27 17:22:35 +0530

    Implement signup and login page ui

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Logo from "@/assets/leetdevlogo.svg";
+import { LoginForm } from "@/components/login-form";
+import Login from "../assets/login.png";
 
 function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="/" className="flex items-center gap-2 font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <img src={Logo} alt="leetdev" className="size-8" />
+            </div>
+            LeetDev.
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src={Login}
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
 }
-
-export default LoginPage
+export default LoginPage;
