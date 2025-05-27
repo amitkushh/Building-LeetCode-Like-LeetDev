ab3fa0597c62dda134ecaced24b6c856d220fbd6
Amit Kushwaha <amitcoding56@gmail.com>
2025-05-27 09:00:28 +0530

    Implement Playlist route

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import problemRoutes from "./routes/problem.routes.js";
 import executionRoute from "./routes/executeCode.routes.js";
 import submissionRoutes from "./routes/submission.routes.js";
+import playlistRoutes from "./routes/playlist.routes.js";
 
 dotenv.config();
 const app = express();
@@ -22,6 +23,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/problems", problemRoutes);
 app.use("/api/v1/execute-code", executionRoute);
 app.use("/api/v1/submission", submissionRoutes);
+app.use("/api/v1/playlist", playlistRoutes)
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
---
+++ backend/src/routes/playlist.routes.js
@@ -0,0 +1,29 @@
+import express from "express";
+import {
+  getPlayAllListDetails,
+  getPlayListDetails,
+  deletePlayList,
+  createPlayList,
+  addProblemToPlaylist,
+  removeProblemFromPlaylist,
+} from "../controllers/playlist.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+
+const playlistRoutes = express.Router();
+
+playlistRoutes.get("/", authMiddleware, getPlayAllListDetails);
+playlistRoutes.get("/:playlistId", authMiddleware, getPlayListDetails);
+playlistRoutes.post("/create-playlist", authMiddleware, createPlayList);
+playlistRoutes.post(
+  "/:playlistId/add-problem",
+  authMiddleware,
+  addProblemToPlaylist
+);
+playlistRoutes.delete("/:playlistId", authMiddleware, deletePlayList);
+playlistRoutes.delete(
+  "/:playlistId/remove-problem",
+  authMiddleware,
+  removeProblemFromPlaylist
+);
+
+export default playlistRoutes;
